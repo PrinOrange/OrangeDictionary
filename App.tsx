478dcf9a0e2622d406bdc8218338a7c9580d4136
Edward Zhang <643431636@qq.com>
2023-02-20 19:31:13 +0800

    add in ui-kitten package

--- App.tsx
+++ App.tsx
@@ -1,10 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-import React from 'react';
+import React from "react";
 import {
   Alert,
   Button,
@@ -13,17 +7,17 @@ import {
   StatusBar,
   Text,
   View,
-} from 'react-native';
+} from "react-native";
 
 function App(): JSX.Element {
   return (
     <SafeAreaView>
       <StatusBar />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Text style={{color: 'red'}}>{'This is my First Android App'}</Text>
+        <Text style={{ color: "red" }}>{"This is my First Android App"}</Text>
         <View>
           <Text>Orange Dictionary</Text>
-          <Button title="aaa" onPress={() => Alert.alert('hello')} />
+          <Button title="aaa" onPress={() => Alert.alert("hello")} />
         </View>
       </ScrollView>
     </SafeAreaView>
